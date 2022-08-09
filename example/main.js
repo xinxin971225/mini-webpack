7b3823456e8df343af1298c73806ab420070caeb
zhiyu <xin872906473@163.com>
2022-08-09 22:54:45 +0800

    createGraph 奖励依赖图关系

--- example/main.js
+++ example/main.js
@@ -1,5 +1,4 @@
 import { foo } from "./foo.js";
-import { bar } from "./bar.js";
 foo();
 export function main() {
   console.log("main");
--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import fs from "fs";
+import path from "path";
 import { parse } from "@babel/parser";
 import traverse from "@babel/traverse";
-function createAsset() {
+function createAsset(filePath) {
   // 获取数据源
-  const source = fs.readFileSync("./example/main.js", {
+  const source = fs.readFileSync(filePath, {
     encoding: "utf-8",
   });
   //   转化为抽象语法树
@@ -25,4 +26,17 @@ function createAsset() {
   };
 }
 
-console.log(createAsset());
+// 创建图关系
+function createGraph() {
+  const mainAsset = createAsset("./example/main.js");
+  const queue = [mainAsset];
+  for (const asset of queue) {
+    asset.deps.forEach((relativePath) => {
+      const child = createAsset(path.resolve("./example", relativePath));
+      queue.push(child);
+    });
+  }
+  return queue;
+}
+
+console.log(createGraph());
